375de568f19f256978d1875d442b1e3f45a12095
kapil-goel952 <goelk849@gmail.com>
2026-08-29 23:38:49 +0530

    added a new function of saving nots of AI

--- app.py
+++ app.py
@@ -367,4 +367,4 @@ elif option == "🤖 Chat with AI":
 
             st.subheader("AI Response")
             st.write(answer)
-            # sdfcdfcsdcssdcsdcdcadfg
+            # sdfcdfcsdcssdcsdcdcadfgkl;'''l;kl''kl
